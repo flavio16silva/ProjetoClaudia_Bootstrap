442be91ceae918bbc82ae3492bf690c28f5e4c7e
flavio16silva <fal_silva@hotmail.com>
2024-08-09 17:01:39 -0300

    criação script para escrita do texto

---
+++ scripts/escrita.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".escrever-texto", {
+      strings: [" avaliação para manuseio de armas de fogo.", "seleção e treinamento.", "avaliação de desempenho."],
+      loop: true,
+      typeSpeed: 50,
+      backSpeed: 25,
+      backDelay: 500
+    });
+  });
