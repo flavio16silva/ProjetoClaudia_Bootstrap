009081f90ef9782c4a520e1f50403ec903c5a4c6
flavio16silva <fal_silva@hotmail.com>
2024-08-17 14:30:20 -0300

    inclusao de frase no script - sessao sobre

--- scripts/escrita.js
+++ scripts/escrita.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     var typed = new Typed(".escrever-texto", {
-      strings: [" avaliação para manuseio de armas de fogo.", "seleção e treinamento.", "avaliação de desempenho."],
+      strings: [" avaliação para manuseio em armas de fogo.", "recrutamento e seleção.", "treinamento e palestras.", "avaliação de desempenho."],
       loop: true,
       typeSpeed: 50,
       backSpeed: 25,
