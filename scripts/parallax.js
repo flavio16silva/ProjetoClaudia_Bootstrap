22b511ba2431b0d140f956dafeb89a0d7fddb67a
flavio16silva <fal_silva@hotmail.com>
2024-08-19 07:09:57 -0300

    cript parallax para sessao estrutura

---
+++ scripts/parallax.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+
+    setTimeout(function() {
+
+        $('#atend1-img').parallax({imageSrc: 'assets/parallax/atendimento1.jpeg'})
+        //$('#atend2-img').parallax({imageSrc: 'assets/parallax/atendimento2.jpeg'})
+    
+      }, 250)
+})
